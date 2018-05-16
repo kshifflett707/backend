18ad5f56c87b6badb87a93611e1af5f74e6ab40e
Kyle Shifflett <kshifflett707@gmail.com>
2018-05-16 10:40:10 -0700

    Fixed last merge conflict

--- src/server/controller/post.js
+++ src/server/controller/post.js
@@ -2,22 +2,13 @@ import db from '../../db/models'
 
 const Post = db.Post;
 
-<<<<<<< HEAD
-const PostTypeError = (expectedType, type) =>
-  Error(`expected post of type ${expectedType} got a post of type ${type}`);
-
-// may never get used
-export const allPosts = (req, res) =>
-  Post.getAllPosts().then((posts) => res.json(posts));
-=======
 const PostTypeError = (expectedType, type) => new Error(`expected post of type ${expectedType} got a post of type ${type}`);
 
 // may never get used
-export const allPosts = (req, res) => 
+export const allPosts = (req, res) =>
   Post.getPostsByQuery()
   .then((posts) => res.status(200).json(posts))
   .catch((err) => res.status(404).json(err));
->>>>>>> d51bfc78844847fcae9f066a2f2eb9c10fb0af75
 
 export const byQuery = (req, res) =>
   Post.getPostsByQuery(req.query)
@@ -29,13 +20,8 @@ export const postsByType =(postType) => (req, res) =>
   .then((posts) => res.status(200).json(posts))
   .catch((err) => res.status(404).json(err));
 
-<<<<<<< HEAD
 export const postById = (postType) => (req, res) =>
-  Post.getPostById(req.params.qid)
-=======
-export const postById = (postType) => (req, res) => 
   Post.getPostById(req.params.id)
->>>>>>> d51bfc78844847fcae9f066a2f2eb9c10fb0af75
   .then((post) => {
     if (post.type !== postType) {
       res.status(404).json(PostTypeError(postType, post.type));
